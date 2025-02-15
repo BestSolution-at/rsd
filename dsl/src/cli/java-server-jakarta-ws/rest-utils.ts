01f9ffe63a0f8fa2bf61ca803de32775471e5fc7
Tom Schindl <tom.schindl@bestsolution.at>
2025-02-15 13:52:51 +0100

    generate rest-helper

---
+++ dsl/src/cli/java-server-jakarta-ws/rest-utils.ts
@@ -0,0 +1,87 @@
+import { CompositeGeneratorNode, NL, toString } from 'langium/generate';
+import { Artifact } from '../artifact-generator.js';
+import {
+  generateCompilationUnit,
+  JavaImportsCollector,
+  JavaServerJakartaWSGeneratorConfig,
+  toPath,
+} from '../java-gen-utils.js';
+
+export function generateRestUtils(
+  artifactConfig: JavaServerJakartaWSGeneratorConfig
+): Artifact {
+  const packageName = `${artifactConfig.rootPackageName}.jdkhttp.impl.model`;
+
+  const importCollector = new JavaImportsCollector(packageName);
+  const fqn = importCollector.importType.bind(importCollector);
+
+  const node = generateRestUtilsContent(artifactConfig, packageName, fqn);
+
+  return {
+    name: '_RestUtils.java',
+    content: toString(
+      generateCompilationUnit(packageName, importCollector, node),
+      '\t'
+    ),
+    path: toPath(artifactConfig.targetFolder, packageName),
+  };
+}
+
+function generateRestUtilsContent(
+  artifactConfig: JavaServerJakartaWSGeneratorConfig,
+  packageName: string,
+  fqn: (type: string) => string
+) {
+  fqn(`${packageName}.model._JsonUtils`);
+  fqn(`${artifactConfig.rootPackageName}.service._RSDException`);
+  fqn('jakarta.ws.rs.core.Response');
+  const node = new CompositeGeneratorNode();
+  node.append('public class _RestUtils {', NL);
+  node.indent((classBody) => {
+    classBody.append(toResponse());
+  });
+  node.append('}', NL);
+  return node;
+}
+
+function toResponse() {
+  const node = new CompositeGeneratorNode();
+  node.append(
+    'public static Response toResponse(int status, _RSDException e) {',
+    NL
+  );
+  node.indent((methodBody) => {
+    methodBody.append(
+      'if (e instanceof _RSDException.RSDMessageException m) {',
+      NL
+    );
+    methodBody.indent((block) => {
+      block.append(
+        'return Response.status(status).entity(m.message).build();',
+        NL
+      );
+    });
+    methodBody.append(
+      '} else if (e instanceof _RSDException.RSDStructuredDataException s) {',
+      NL
+    );
+    methodBody.indent((block) => {
+      block.append(
+        'return Response.status(status).entity(_JsonUtils.toJsonString(s.data, false)).build();',
+        NL
+      );
+    });
+    methodBody.append('}', NL);
+  });
+  node.append('return Response.serverError().build();', NL);
+  node.append('}');
+
+  return node;
+}
+
+// if (e instanceof _RSDException.RSDMessageException m) {
+//   return Response.status(status).entity(m.message).build();
+// } else if (e instanceof _RSDException.RSDStructuredDataException s) {
+//   return Response.status(status).entity(_JsonUtils.toJsonString(s.data, false)).build();
+// }
+// return Response.serverError().build();
