7b59e2dc7f90ff425f32b91ea174a027aa641ee1
Tom Schindl <tom.schindl@bestsolution.at>
2025-04-11 15:29:54 +0200

    implement first version for darkmode

--- webcomponent/src/components/rsd-preview-alt/rsd-preview-alt.tsx
+++ webcomponent/src/components/rsd-preview-alt/rsd-preview-alt.tsx
@@ -24,7 +24,7 @@ function serviceContent(model: MService) {
       <div class="mb-14 inner-content">
         <p>{model.doc}</p>
         <div class="not-prose">
-          <div class="bg-zinc-900 rounded-2xl">
+          <div class="bg-zinc-900 rounded-2xl dark:ring-1 dark:ring-white/10 m-1">
             <div class="p-4 rounded-t-2xl border-b border-zinc-700 bg-zinc-800">
               <h3 class="text-xs text-white font-semibold">Definition</h3>
               {/*<div>
@@ -74,7 +74,7 @@ function serviceMethod(model: MOperation) {
           {model.resultType !== undefined && result(model.resultType)}
         </div>
         <div class="code-column not-prose">
-          <div class="bg-zinc-900 rounded-2xl">
+          <div class="bg-zinc-900 rounded-2xl dark:ring-1 dark:ring-white/10 m-1">
             <div class="p-4 rounded-t-2xl border-b border-zinc-700 bg-zinc-800">
               <h3 class="text-xs text-white font-semibold">Definition</h3>
               {/*<div>
@@ -200,7 +200,7 @@ function parameter(p: MParameter) {
     <li class="not-prose py-4 first:pt-0 last:pb-0">
       <dl class="parameter">
         <dd>
-          <code class="bg-zinc-700/5 text-xs p-1 rounded-lg border border-zinc-300">{p.name}</code>
+          <code class="bg-zinc-700/5 dark:bg-zinc-700/15 border-zinc-300 dark:border-zinc-700 dark:text-white text-xs p-1 rounded-lg border">{p.name}</code>
         </dd>
         <dd class="font-mono text-xs text-zinc-400">
           {p.array ? 'Array<' : ''}
@@ -347,9 +347,8 @@ export class RSDPreviewAlt {
   @State()
   activeContext: string;
 
-  private foo = () => {
-    console.log('CLICKKED');
-  };
+  @State()
+  theme: string;
 
   render() {
     let content = 'Nothing';
@@ -363,17 +362,22 @@ export class RSDPreviewAlt {
       }
     }
 
+    const handleClick = () => {
+      this.theme = this.theme === 'dark' ? 'light' : 'dark';
+      document.documentElement.style.colorScheme = this.theme;
+    };
+
     return (
       <Fragment>
-        <div class="flex flex-grow lg:ml-72 xl:ml-80">
+        <div class={`bg-white dark:bg-zinc-900 flex flex-grow lg:ml-72 xl:ml-80 ${this.theme ?? ''}`}>
           <header class="contents lg:fixed lg:inset-0 lg:flex lg:pointer-events-none">
-            <div class="contents lg:block lg:w-72 xl:w-80 border-zinc-900/10 lg:border-r lg:overflow-y-auto bg-gray-50 lg:pointer-events-auto lg:px-6 lg:pt-4 lg:pb-8 lg:dark:border-white/10">
+            <div class="bg-gray-50 dark:bg-zinc-800 border-zinc-900/10 lg:dark:border-white/10 contents lg:block lg:w-72 xl:w-80 lg:border-r lg:overflow-y-auto lg:pointer-events-auto lg:px-6 lg:pt-4 lg:pb-8">
               <div class="hidden lg:block">
                 <a class="text-xl font-bold" href="#home">
                   {this.projectname}
                 </a>
               </div>
-              <div class="items-center gap-12 justify-between px-4 sm:px-6 lg:px-8 fixed inset-0 h-14 lg:left-72 xl:left-80 backdrop-blur-xs border-b border-zinc-900/10 bg-white/60 flex">
+              <div class="bg-white/60 dark:bg-zinc-900/60 border-zinc-900/10 dark:border-white/10 items-center gap-12 justify-between px-4 sm:px-6 lg:px-8 fixed inset-0 h-14 lg:left-72 xl:left-80 backdrop-blur-xs border-b flex">
                 <div class="lg:hidden">
                   <a class="text-xl font-bold" href="#home">
                     {this.projectname}
@@ -391,18 +395,20 @@ export class RSDPreviewAlt {
                     </kbd>
                   </button>
                 </div>
-                {/*<div class="flex items-center gap-5">
-                  <button onClick={handleClick}>
-                    <svg class="h-3 w-3 fill-zinc-400 dark:hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                      <path d="M375.7 19.7c-1.5-8-6.9-14.7-14.4-17.8s-16.1-2.2-22.8 2.4L256 61.1 173.5 4.2c-6.7-4.6-15.3-5.5-22.8-2.4s-12.9 9.8-14.4 17.8l-18.1 98.5L19.7 136.3c-8 1.5-14.7 6.9-17.8 14.4s-2.2 16.1 2.4 22.8L61.1 256 4.2 338.5c-4.6 6.7-5.5 15.3-2.4 22.8s9.8 13 17.8 14.4l98.5 18.1 18.1 98.5c1.5 8 6.9 14.7 14.4 17.8s16.1 2.2 22.8-2.4L256 450.9l82.5 56.9c6.7 4.6 15.3 5.5 22.8 2.4s12.9-9.8 14.4-17.8l18.1-98.5 98.5-18.1c8-1.5 14.7-6.9 17.8-14.4s2.2-16.1-2.4-22.8L450.9 256l56.9-82.5c4.6-6.7 5.5-15.3 2.4-22.8s-9.8-12.9-17.8-14.4l-98.5-18.1L375.7 19.7zM269.6 110l65.6-45.2 14.4 78.3c1.8 9.8 9.5 17.5 19.3 19.3l78.3 14.4L402 242.4c-5.7 8.2-5.7 19 0 27.2l45.2 65.6-78.3 14.4c-9.8 1.8-17.5 9.5-19.3 19.3l-14.4 78.3L269.6 402c-8.2-5.7-19-5.7-27.2 0l-65.6 45.2-14.4-78.3c-1.8-9.8-9.5-17.5-19.3-19.3L64.8 335.2 110 269.6c5.7-8.2 5.7-19 0-27.2L64.8 176.8l78.3-14.4c9.8-1.8 17.5-9.5 19.3-19.3l14.4-78.3L242.4 110c8.2 5.7 19 5.7 27.2 0zM256 368a112 112 0 1 0 0-224 112 112 0 1 0 0 224zM192 256a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
-                    </svg>
-                    <svg class="hidden h-3 w-3 fill-white dark:block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
-                      <path d="M144.7 98.7c-21 34.1-33.1 74.3-33.1 117.3c0 98 62.8 181.4 150.4 211.7c-12.4 2.8-25.3 4.3-38.6 4.3C126.6 432 48 353.3 48 256c0-68.9 39.4-128.4 96.8-157.3zm62.1-66C91.1 41.2 0 137.9 0 256C0 379.7 100 480 223.5 480c47.8 0 92-15 128.4-40.6c1.9-1.3 3.7-2.7 5.5-4c4.8-3.6 9.4-7.4 13.9-11.4c2.7-2.4 5.3-4.8 7.9-7.3c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-3.7 .6-7.4 1.2-11.1 1.6c-5 .5-10.1 .9-15.3 1c-1.2 0-2.5 0-3.7 0l-.3 0c-96.8-.2-175.2-78.9-175.2-176c0-54.8 24.9-103.7 64.1-136c1-.9 2.1-1.7 3.2-2.6c4-3.2 8.2-6.2 12.5-9c3.1-2 6.3-4 9.6-5.8c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-3.6-.3-7.1-.5-10.7-.6c-2.7-.1-5.5-.1-8.2-.1c-3.3 0-6.5 .1-9.8 .2c-2.3 .1-4.6 .2-6.9 .4z" />
-                    </svg>
-                  </button>
-                </div>*/}
+                {
+                  <div class="flex items-center gap-5">
+                    <button onClick={handleClick}>
+                      <svg class="h-3 w-3 fill-zinc-400 dark:hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path d="M375.7 19.7c-1.5-8-6.9-14.7-14.4-17.8s-16.1-2.2-22.8 2.4L256 61.1 173.5 4.2c-6.7-4.6-15.3-5.5-22.8-2.4s-12.9 9.8-14.4 17.8l-18.1 98.5L19.7 136.3c-8 1.5-14.7 6.9-17.8 14.4s-2.2 16.1 2.4 22.8L61.1 256 4.2 338.5c-4.6 6.7-5.5 15.3-2.4 22.8s9.8 13 17.8 14.4l98.5 18.1 18.1 98.5c1.5 8 6.9 14.7 14.4 17.8s16.1 2.2 22.8-2.4L256 450.9l82.5 56.9c6.7 4.6 15.3 5.5 22.8 2.4s12.9-9.8 14.4-17.8l18.1-98.5 98.5-18.1c8-1.5 14.7-6.9 17.8-14.4s2.2-16.1-2.4-22.8L450.9 256l56.9-82.5c4.6-6.7 5.5-15.3 2.4-22.8s-9.8-12.9-17.8-14.4l-98.5-18.1L375.7 19.7zM269.6 110l65.6-45.2 14.4 78.3c1.8 9.8 9.5 17.5 19.3 19.3l78.3 14.4L402 242.4c-5.7 8.2-5.7 19 0 27.2l45.2 65.6-78.3 14.4c-9.8 1.8-17.5 9.5-19.3 19.3l-14.4 78.3L269.6 402c-8.2-5.7-19-5.7-27.2 0l-65.6 45.2-14.4-78.3c-1.8-9.8-9.5-17.5-19.3-19.3L64.8 335.2 110 269.6c5.7-8.2 5.7-19 0-27.2L64.8 176.8l78.3-14.4c9.8-1.8 17.5-9.5 19.3-19.3l14.4-78.3L242.4 110c8.2 5.7 19 5.7 27.2 0zM256 368a112 112 0 1 0 0-224 112 112 0 1 0 0 224zM192 256a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z" />
+                      </svg>
+                      <svg class="hidden h-3 w-3 fill-white dark:block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                        <path d="M144.7 98.7c-21 34.1-33.1 74.3-33.1 117.3c0 98 62.8 181.4 150.4 211.7c-12.4 2.8-25.3 4.3-38.6 4.3C126.6 432 48 353.3 48 256c0-68.9 39.4-128.4 96.8-157.3zm62.1-66C91.1 41.2 0 137.9 0 256C0 379.7 100 480 223.5 480c47.8 0 92-15 128.4-40.6c1.9-1.3 3.7-2.7 5.5-4c4.8-3.6 9.4-7.4 13.9-11.4c2.7-2.4 5.3-4.8 7.9-7.3c5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-3.7 .6-7.4 1.2-11.1 1.6c-5 .5-10.1 .9-15.3 1c-1.2 0-2.5 0-3.7 0l-.3 0c-96.8-.2-175.2-78.9-175.2-176c0-54.8 24.9-103.7 64.1-136c1-.9 2.1-1.7 3.2-2.6c4-3.2 8.2-6.2 12.5-9c3.1-2 6.3-4 9.6-5.8c6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-3.6-.3-7.1-.5-10.7-.6c-2.7-.1-5.5-.1-8.2-.1c-3.3 0-6.5 .1-9.8 .2c-2.3 .1-4.6 .2-6.9 .4z" />
+                      </svg>
+                    </button>
+                  </div>
+                }
               </div>
-              <nav class="prose hidden lg:block">
+              <nav class="prose dark:prose-invert hidden lg:block">
                 <ul class="list-none p-0">
                   <li>
                     <h2 class="text-sm font-semibold text-zinc-900 dark:text-white">Services</h2>
@@ -432,7 +438,7 @@ export class RSDPreviewAlt {
           </header>
           {/* contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pt-4 lg:pb-8 xl:w-80 lg:dark:border-white/10 */}
           {/**/}
-          <main class="flex prose">
+          <main class="flex prose dark:prose-invert">
             <div class="w-full">{content}</div>
           </main>
         </div>
