9971694bba1c66ec0778c0ca07e69317f6625411
Tom Schindl <tom.schindl@bestsolution.at>
2025-04-18 18:07:43 +0200

    remove old preview

--- webcomponent/src/components.d.ts
+++ webcomponent/src/components.d.ts
@@ -8,22 +8,12 @@ import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 import { MRSDModel } from "./utils/model";
 export { MRSDModel } from "./utils/model";
 export namespace Components {
-    interface RsdPreview {
-        "model": string | MRSDModel;
-        "projectname": string;
-    }
     interface RsdPreviewAlt {
         "model": string | MRSDModel;
         "projectname": string;
     }
 }
 declare global {
-    interface HTMLRsdPreviewElement extends Components.RsdPreview, HTMLStencilElement {
-    }
-    var HTMLRsdPreviewElement: {
-        prototype: HTMLRsdPreviewElement;
-        new (): HTMLRsdPreviewElement;
-    };
     interface HTMLRsdPreviewAltElement extends Components.RsdPreviewAlt, HTMLStencilElement {
     }
     var HTMLRsdPreviewAltElement: {
@@ -31,21 +21,15 @@ declare global {
         new (): HTMLRsdPreviewAltElement;
     };
     interface HTMLElementTagNameMap {
-        "rsd-preview": HTMLRsdPreviewElement;
         "rsd-preview-alt": HTMLRsdPreviewAltElement;
     }
 }
 declare namespace LocalJSX {
-    interface RsdPreview {
-        "model"?: string | MRSDModel;
-        "projectname"?: string;
-    }
     interface RsdPreviewAlt {
         "model"?: string | MRSDModel;
         "projectname"?: string;
     }
     interface IntrinsicElements {
-        "rsd-preview": RsdPreview;
         "rsd-preview-alt": RsdPreviewAlt;
     }
 }
@@ -53,7 +37,6 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "rsd-preview": LocalJSX.RsdPreview & JSXBase.HTMLAttributes<HTMLRsdPreviewElement>;
             "rsd-preview-alt": LocalJSX.RsdPreviewAlt & JSXBase.HTMLAttributes<HTMLRsdPreviewAltElement>;
         }
     }
