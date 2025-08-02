ec36cc2df841015cb21863d313340ebbaf989cd6
Tom Schindl <tom.schindl@bestsolution.at>
2025-08-02 18:56:55 +0200

    fixed sample generator location

--- dsl/gen-sample.sh
+++ dsl/gen-sample.sh
@@ -1,5 +1,5 @@
 npm run langium:generate \
  && npm run build
 
-node ./bin/cli.js model examples/simple/simple-sample.rsd -d examples/generated
+node ./bin/cli.js model examples/simple/simple-sample.rsd -d examples/simple/generated
 node ./bin/cli.js artifacts examples/simple/simple-sample.rsd -c examples/simple/.rsd-generator.json
