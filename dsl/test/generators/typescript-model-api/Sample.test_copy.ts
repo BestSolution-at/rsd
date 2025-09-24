400e062c796833b8c50bd9cc2ea01e559d16025e
Tom Schindl <tom.schindl@bestsolution.at>
2025-09-24 11:50:55 +0200

    started adding tests for Basic types

---
+++ dsl/test/generators/typescript-model-api/Sample.test_copy.ts
@@ -0,0 +1,18 @@
+import { describe, expect, test } from 'vitest';
+
+describe('SimpleRecord_BasicFromJSON', () => {
+	test('simple', () => {});
+	test('remove-unknown', () => {});
+	test('missing prop', () => {});
+	test('invalid prop value', () => {});
+});
+describe('isSimpleRecord_Basic', () => {
+	test('simple', () => {});
+	test('additional props', () => {});
+	test('missing props', () => {});
+	test('invalid props', () => {});
+});
+describe('SimpleRecord_BasicToJSON', () => {
+	test('simple', () => {});
+	test('additional props', () => {});
+});
