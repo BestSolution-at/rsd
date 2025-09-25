a1543c62e861422f383f6a0f9748cf756eef30d9
Tom Schindl <tom.schindl@bestsolution.at>
2025-09-25 18:56:47 +0200

    added more helper methods

--- dsl/packages/cli/test/generated-code/typescript-model-api/utils.ts
+++ dsl/packages/cli/test/generated-code/typescript-model-api/utils.ts
@@ -1,3 +1,20 @@
 export function addFooProperty<T>(value: T): T {
 	return { ...value, foo: 'bar' };
 }
+
+export function removeProperty(value: Record<string, unknown>, prop: string): { withOut: Record<string, unknown>; withUndefined: Record<string, unknown> } {
+	const withOut = { ...value };
+	// eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+	delete withOut[prop];
+	const withUndefined = { ...value };
+	withUndefined[prop] = undefined;
+	return { withOut, withUndefined };
+}
+
+const Invalid = Symbol('Invalid');
+
+export function invalidateProperty(value: Record<string, unknown>, prop: string): Record<string, unknown> {
+	const copy = { ...value };
+	copy[prop] = Invalid;
+	return copy;
+}
