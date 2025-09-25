db4b6f0670a818dd00aea5b2da5cf8d4ed8663de
Tom Schindl <tom.schindl@bestsolution.at>
2025-09-25 23:05:18 +0200

    fix patch generation for ints

--- dsl/packages/cli/src/java-model-api/shared.ts
+++ dsl/packages/cli/src/java-model-api/shared.ts
@@ -1,203 +1,100 @@
 import { CompositeGeneratorNode, NL } from 'langium/generate';
-import {
-  isMProperty,
-  MResolvedBaseProperty,
-  MResolvedPropery,
-} from '../model.js';
+import { isMProperty, MResolvedBaseProperty, MResolvedPropery } from '../model.js';
 import { computeAPIType, primitiveToObject } from '../java-gen-utils.js';
 import { toFirstUpper } from '../util.js';
 
-export function generatePropertyAccessor(
-  property: MResolvedBaseProperty,
-  nativeTypeSubstitues: Record<string, string> | undefined,
-  basePackageName: string,
-  fqn: (type: string) => string
-) {
-  const node = new CompositeGeneratorNode();
-  node.append(
-    `public ${computeAPIType(
-      property,
-      nativeTypeSubstitues,
-      basePackageName,
-      fqn
-    )} ${property.name}();`,
-    NL
-  );
-  return node;
+export function generatePropertyAccessor(property: MResolvedBaseProperty, nativeTypeSubstitues: Record<string, string> | undefined, basePackageName: string, fqn: (type: string) => string) {
+	const node = new CompositeGeneratorNode();
+	node.append(`public ${computeAPIType(property, nativeTypeSubstitues, basePackageName, fqn)} ${property.name}();`, NL);
+	return node;
 }
 
-export function generateBuilderPropertyAccessor(
-  property: MResolvedBaseProperty,
-  nativeTypeSubstitues: Record<string, string> | undefined,
-  basePackageName: string,
-  fqn: (type: string) => string,
-  returnType = 'DataBuilder'
-) {
-  const node = new CompositeGeneratorNode();
-  node.append(
-    `public ${returnType} ${property.name}(${computeAPIType(
-      property,
-      nativeTypeSubstitues,
-      basePackageName,
-      fqn
-    )} ${property.name});`,
-    NL
-  );
-  if (
-    isMProperty(property) &&
-    !property.array &&
-    (property.variant === 'record' || property.variant === 'union')
-  ) {
-    const Function = fqn('java.util.function.Function');
-    node.append(
-      NL,
-      `public <T extends ${
-        property.type
-      }.DataBuilder> ${returnType} with${toFirstUpper(
-        property.name
-      )}(Class<T> clazz, ${Function}<T, ${property.type}.Data> block);`,
-      NL
-    );
-  }
-  return node;
+export function generateBuilderPropertyAccessor(property: MResolvedBaseProperty, nativeTypeSubstitues: Record<string, string> | undefined, basePackageName: string, fqn: (type: string) => string, returnType = 'DataBuilder') {
+	const node = new CompositeGeneratorNode();
+	node.append(`public ${returnType} ${property.name}(${computeAPIType(property, nativeTypeSubstitues, basePackageName, fqn)} ${property.name});`, NL);
+	if (isMProperty(property) && !property.array && (property.variant === 'record' || property.variant === 'union')) {
+		const Function = fqn('java.util.function.Function');
+		node.append(NL, `public <T extends ${property.type}.DataBuilder> ${returnType} with${toFirstUpper(property.name)}(Class<T> clazz, ${Function}<T, ${property.type}.Data> block);`, NL);
+	}
+	return node;
 }
 
-export function generatePatchPropertyAccessor(
-  property: MResolvedPropery,
-  nativeTypeSubstitues: Record<string, string> | undefined,
-  basePackageName: string,
-  fqn: (type: string) => string
-) {
-  const node = new CompositeGeneratorNode();
-  if (
-    property.variant === 'builtin' ||
-    property.variant === 'enum' ||
-    property.variant === 'inline-enum' ||
-    property.variant === 'scalar'
-  ) {
-    let type = primitiveToObject(
-      computeAPIType(property, nativeTypeSubstitues, basePackageName, fqn, true)
-    );
+export function generatePatchPropertyAccessor(property: MResolvedPropery, nativeTypeSubstitues: Record<string, string> | undefined, basePackageName: string, fqn: (type: string) => string) {
+	const node = new CompositeGeneratorNode();
+	if (property.variant === 'builtin' || property.variant === 'enum' || property.variant === 'inline-enum' || property.variant === 'scalar') {
+		let type = primitiveToObject(computeAPIType(property, nativeTypeSubstitues, basePackageName, fqn, true));
 
-    if (property.array) {
-      type = `${toFirstUpper(property.name)}Change`;
-    }
+		if (property.array) {
+			type = `${toFirstUpper(property.name)}Change`;
+		}
 
-    if (property.optional || property.nullable) {
-      node.append(`public _Base.Nillable<${type}> ${property.name}();`, NL);
-    } else {
-      const Optional = fqn('java.util.Optional');
-      node.append(`public ${Optional}<${type}> ${property.name}();`, NL);
-    }
-  } else {
-    let type = computeAPIType(
-      property,
-      nativeTypeSubstitues,
-      basePackageName,
-      fqn,
-      true
-    );
+		if (property.optional || property.nullable) {
+			node.append(`public _Base.Nillable<${type}> ${property.name}();`, NL);
+		} else {
+			if (type === 'Integer') {
+				const Optional = fqn('java.util.OptionalInt');
+				node.append(`public ${Optional} ${property.name}();`, NL);
+			} else if (type === 'Long') {
+				const Optional = fqn('java.util.OptionalLong');
+				node.append(`public ${Optional} ${property.name}();`, NL);
+			} else if (type === 'Double') {
+				const Optional = fqn('java.util.OptionalLong');
+				node.append(`public ${Optional} ${property.name}();`, NL);
+			} else {
+				const Optional = fqn('java.util.Optional');
+				node.append(`public ${Optional}<${type}> ${property.name}();`, NL);
+			}
+		}
+	} else {
+		let type = computeAPIType(property, nativeTypeSubstitues, basePackageName, fqn, true);
 
-    if (property.array) {
-      type = `${toFirstUpper(property.name)}Change`;
-    } else {
-      type = `${basePackageName}.${property.type}`;
-    }
+		if (property.array) {
+			type = `${toFirstUpper(property.name)}Change`;
+		} else {
+			type = `${basePackageName}.${property.type}`;
+		}
 
-    if (property.optional || property.nullable) {
-      node.append(`public _Base.Nillable<${type}> ${property.name}();`, NL);
-    } else {
-      const Optional = fqn('java.util.Optional');
-      node.append(`public ${Optional}<${type}> ${property.name}();`, NL);
-    }
-  }
+		if (property.optional || property.nullable) {
+			node.append(`public _Base.Nillable<${type}> ${property.name}();`, NL);
+		} else {
+			const Optional = fqn('java.util.Optional');
+			node.append(`public ${Optional}<${type}> ${property.name}();`, NL);
+		}
+	}
 
-  return node;
+	return node;
 }
 
-export function generatePatchBuilderPropertyAccessor(
-  property: MResolvedPropery,
-  nativeTypeSubstitues: Record<string, string> | undefined,
-  basePackageName: string,
-  fqn: (type: string) => string
-) {
-  const node = new CompositeGeneratorNode();
-  if (
-    property.variant === 'builtin' ||
-    property.variant === 'enum' ||
-    property.variant === 'inline-enum' ||
-    property.variant === 'scalar'
-  ) {
-    let type = computeAPIType(
-      property,
-      nativeTypeSubstitues,
-      basePackageName,
-      fqn,
-      true
-    );
-    if (property.nullable || property.optional || property.array) {
-      type = primitiveToObject(type);
-    }
+export function generatePatchBuilderPropertyAccessor(property: MResolvedPropery, nativeTypeSubstitues: Record<string, string> | undefined, basePackageName: string, fqn: (type: string) => string) {
+	const node = new CompositeGeneratorNode();
+	if (property.variant === 'builtin' || property.variant === 'enum' || property.variant === 'inline-enum' || property.variant === 'scalar') {
+		let type = computeAPIType(property, nativeTypeSubstitues, basePackageName, fqn, true);
+		if (property.nullable || property.optional || property.array) {
+			type = primitiveToObject(type);
+		}
 
-    if (property.array) {
-      node.append(
-        `public PatchBuilder ${property.name}(Patch.${toFirstUpper(
-          property.name
-        )}Change ${property.name});`,
-        NL
-      );
-      node.append(
-        `public PatchBuilder ${property.name}(List<${type}> additions, List<${type}> removals);`,
-        NL
-      );
-      node.append(
-        `public PatchBuilder ${property.name}(List<${type}> elements);`,
-        NL
-      );
-    } else {
-      node.append(
-        `public PatchBuilder ${property.name}(${type} ${property.name});`,
-        NL
-      );
-    }
-  } else {
-    if (!property.array) {
-      const baseType = fqn(`${basePackageName}.${property.type}`);
-      const Function = fqn('java.util.function.Function');
-      node.append(
-        `public PatchBuilder ${property.name}(${baseType} ${property.name});`,
-        NL
-      );
-      node.append(
-        `public <T extends ${baseType}.Builder> PatchBuilder with${toFirstUpper(
-          property.name
-        )}(Class<T> clazz, ${Function}<T, ${baseType}> block);`,
-        NL
-      );
-    } else {
-      const type = computeAPIType(
-        property,
-        nativeTypeSubstitues,
-        basePackageName,
-        fqn,
-        true
-      );
-      const baseType = fqn(`${basePackageName}.${property.type}`);
-      const prefix = toFirstUpper(property.name);
-      node.append(
-        `public PatchBuilder ${property.name}(Patch.${prefix}Change ${property.name});`,
-        NL
-      );
-      const List = fqn('java.util.List');
-      node.append(
-        `public PatchBuilder ${property.name}(${List}<${type}> additions, ${List}<${baseType}.Patch> updates, ${List}<String> removals);`,
-        NL
-      );
-      node.append(
-        `public PatchBuilder ${property.name}(List<${type}> elements);`,
-        NL
-      );
-    }
-  }
-  return node;
+		if (property.array) {
+			node.append(`public PatchBuilder ${property.name}(Patch.${toFirstUpper(property.name)}Change ${property.name});`, NL);
+			node.append(`public PatchBuilder ${property.name}(List<${type}> additions, List<${type}> removals);`, NL);
+			node.append(`public PatchBuilder ${property.name}(List<${type}> elements);`, NL);
+		} else {
+			node.append(`public PatchBuilder ${property.name}(${type} ${property.name});`, NL);
+		}
+	} else {
+		if (!property.array) {
+			const baseType = fqn(`${basePackageName}.${property.type}`);
+			const Function = fqn('java.util.function.Function');
+			node.append(`public PatchBuilder ${property.name}(${baseType} ${property.name});`, NL);
+			node.append(`public <T extends ${baseType}.Builder> PatchBuilder with${toFirstUpper(property.name)}(Class<T> clazz, ${Function}<T, ${baseType}> block);`, NL);
+		} else {
+			const type = computeAPIType(property, nativeTypeSubstitues, basePackageName, fqn, true);
+			const baseType = fqn(`${basePackageName}.${property.type}`);
+			const prefix = toFirstUpper(property.name);
+			node.append(`public PatchBuilder ${property.name}(Patch.${prefix}Change ${property.name});`, NL);
+			const List = fqn('java.util.List');
+			node.append(`public PatchBuilder ${property.name}(${List}<${type}> additions, ${List}<${baseType}.Patch> updates, ${List}<String> removals);`, NL);
+			node.append(`public PatchBuilder ${property.name}(List<${type}> elements);`, NL);
+		}
+	}
+	return node;
 }
