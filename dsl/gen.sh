64de34d292a33b3fa73f3213236c92684f9363cd
Tom Schindl <tom.schindl@bestsolution.at>
2026-03-03 21:00:13 +0100

    fix gen

--- dsl/gen.sh
+++ dsl/gen.sh
@@ -2,4 +2,4 @@ npm run langium:generate \
  && npm run build
 
 node ./packages/cli/bin/cli.js model ../../qutime/spec/quti.rsd -d ../../qutime/spec/
-node ./packages/cli/bin/cli.js ../../qutime/spec/quti.rsd -c ../../qutime/spec/.rsd-generator.json
+node ./packages/cli/bin/cli.js artifacts ../../qutime/spec/quti.rsd -c ../../qutime/spec/.rsd-generator.json
