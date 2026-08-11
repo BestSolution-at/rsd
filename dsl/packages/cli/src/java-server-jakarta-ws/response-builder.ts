4213a06f2471da3b16cb2e331dad32ac6c29d70f
Tom Schindl <tom.schindl@bestsolution.at>
2026-08-11 18:03:59 +0200

    92 - force newline endings

--- dsl/packages/cli/src/java-server-jakarta-ws/response-builder.ts
+++ dsl/packages/cli/src/java-server-jakarta-ws/response-builder.ts
@@ -77,6 +77,7 @@ function generateContent(
 							const content = toNodeTree(`
 							return ${RestMulti}.fromMultiData($result.map(e -> ${JsonUtils}.encodeValue(${_Support}.${o.resultType.type}ToJson(e), $contentType, /* FIXME */ null)))
 								.header("Content-Type", $contentType)
+								.encodeAsJsonArray(false)
 								.status(${code.toFixed()});
 							`);
 							methodBody.append(content);
@@ -85,6 +86,7 @@ function generateContent(
 							const content = toNodeTree(`
 							return ${RestMulti}.fromMultiData($result.map(e -> ${JsonUtils}.encodeValue(e, $contentType, /* FIXME */ null)))
 								.header("Content-Type", $contentType)
+								.encodeAsJsonArray(false)
 								.status(${code.toFixed()});
 							`);
 							methodBody.append(content);
